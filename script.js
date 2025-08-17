1a1993c61fa97ec66c5af612f50e1d87bf36489f
thienquang-123 <thienquang120396@gmail.com>
2025-08-17 21:11:21 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,135 +1,39 @@
 // --- VOCABULARY LIST ---
 const vocab = [
-    { chinese: "你好", pinyin: "nǐ hǎo", english: ["hello", "hi"], vietnamese: ["xin chào", "chào bạn"] },
-    { chinese: "谢谢", pinyin: "xièxie", english: ["thank you", "thanks"], vietnamese: ["cảm ơn", "cám ơn"] },
-    { chinese: "不客气", pinyin: "bú kèqi", english: ["you're welcome", "you are welcome", "not at all"], vietnamese: ["không có gì", "đừng khách sáo"] },
-    { chinese: "再见", pinyin: "zàijiàn", english: ["goodbye", "bye"], vietnamese: ["tạm biệt"] },
-    { chinese: "对不起", pinyin: "duìbuqǐ", english: ["sorry", "excuse me"], vietnamese: ["xin lỗi"] },
-    { chinese: "没关系", pinyin: "méi guānxi", english: ["it's ok", "it doesn't matter", "no problem"], vietnamese: ["không sao", "không có gì"] },
-    { chinese: "是", pinyin: "shì", english: ["yes", "is", "are", "am"], vietnamese: ["là", "phải", "đúng"] },
-    { chinese: "我", pinyin: "wǒ", english: ["i", "me"], vietnamese: ["tôi", "tớ"] },
-    { chinese: "我的", pinyin: "wǒ de", english: ["my", "mine"], vietnamese: ["của tôi"] },
-    { chinese: "你", pinyin: "nǐ", english: ["you"], vietnamese: ["bạn", "cậu"] },
-    { chinese: "你的", pinyin: "nǐ de", english: ["your", "yours"], vietnamese: ["của bạn"] },
-    { chinese: "他", pinyin: "tā", english: ["he", "him"], vietnamese: ["anh ấy", "cậu ấy", "ông ấy"] },
-    { chinese: "他的", pinyin: "tā de", english: ["his"], vietnamese: ["của anh ấy"] },
-    { chinese: "她", pinyin: "tā", english: ["she", "her"], vietnamese: ["cô ấy", "chị ấy", "bà ấy"] },
-    { chinese: "她的", pinyin: "tā de", english: ["her", "hers"], vietnamese: ["của cô ấy"] },
-    { chinese: "我们", pinyin: "wǒmen", english: ["we", "us"], vietnamese: ["chúng tôi", "chúng ta"] },
-    { chinese: "爱", pinyin: "ài", english: ["love", "to love"], vietnamese: ["yêu", "thích"] },
-    { chinese: "家", pinyin: "jiā", english: ["home", "family"], vietnamese: ["nhà", "gia đình"] },
-    { chinese: "吃", pinyin: "chī", english: ["eat", "to eat"], vietnamese: ["ăn"] },
-    { chinese: "人", pinyin: "rén", english: ["human", "man"], vietnamese: ["nhân", "người", "con người"] },
-    { chinese: "从", pinyin: "cóng", english: ["from"], vietnamese: ["từ"] },
-    { chinese: "个", pinyin: "ge", english: ["universal measured word"], vietnamese: ["từ dùng để đếm", "cái", "quả", "con"] },
-    { chinese: "个人", pinyin: "gè rén", english: ["individual", "personal"], vietnamese: ["riêng tư", "cá nhân", "một mình"] },
-    { chinese: "大", pinyin: "dà", english: ["big"], vietnamese: ["to", "lớn", "đại"] },
-    { chinese: "天", pinyin: "tiān", english: ["sky", "day"], vietnamese: ["bầu trời", "thiên", "ngày"] },
-    { chinese: "太", pinyin: "tài", english: ["too"], vietnamese: ["quá"] },
-    { chinese: "上", pinyin: "shàng", english: ["up", "above", "on", "over"], vietnamese: ["thượng"] },
-    { chinese: "下", pinyin: "xià", english: ["down", "below", "under"], vietnamese: ["hạ"] },
-    { chinese: "中", pinyin: "zhōng", english: ["middle", "center"], vietnamese: ["trung"] },
-    { chinese: "了", pinyin: "le", english: ["common sentence particle"], vietnamese: ["liễu", "xong", "hết", "đã", "rồi"] },
-    { chinese: "子", pinyin: "zǐ/zi", english: ["child", "suffix character"], vietnamese: ["tử", "con", "cái"] },
-    { chinese: "字", pinyin: "zì", english: ["character", "letter"], vietnamese: ["tự", "chữ"] },
-    { chinese: "口", pinyin: "kǒu", english: ["mouth", "opening"], vietnamese: ["khẩu", "mồm", "miệng", "cửa"] },
-    { chinese: "人口", pinyin: "rén kǒu ", english: ["population"], vietnamese: ["nhân khẩu", "dân số"] },
-    { chinese: "手", pinyin: "shǒu", english: ["hand"], vietnamese: ["thủ", "tay"] },
-    { chinese: "心", pinyin: "xīn", english: ["heart"], vietnamese: ["tâm", "tim", "lòng"] },
-    { chinese: "中心", pinyin: "zhōng xīn", english: ["middle", "center"], vietnamese: ["trung tâm"] },
-    { chinese: "心中", pinyin: "xīn zhōng", english: ["in the heart"], vietnamese: ["trong tim", "trong trái tim"] },
-    { chinese: "开心", pinyin: "kāi xīn", english: ["happy"], vietnamese: ["vui vẻ", "hạnh phúc"] },
-    { chinese: "日", pinyin: "rì", english: ["sun", "day"], vietnamese: ["nhật", "mặt trời", "ngày"] },
-    { chinese: "月", pinyin: "yuè", english: ["moon", "month"], vietnamese: ["nguyệt", "mặt trăng", "tháng"] },
-    { chinese: "明", pinyin: "míng", english: ["bright", "clear"], vietnamese: ["minh", "sáng"] },
-    { chinese: "明天", pinyin: "míng tiān", english: ["tomorrow"], vietnamese: ["ngày mai"] },
-    { chinese: "白", pinyin: "bái", english: ["white"], vietnamese: ["bạch", "trắng", "màu trắng"] },
-    { chinese: "明白", pinyin: "míng bái", english: ["understand", "to understand"], vietnamese: ["minh bạch", "hiểu"] },
-    { chinese: "白天", pinyin: "bái tiān", english: ["daytime"], vietnamese: ["ban ngày"] },
-    { chinese: "百", pinyin: "bǎi", english: ["hundred"], vietnamese: ["bách", "trăm", "rất nhiều"] },
-    { chinese: "的", pinyin: "de", english: ["possessive particle", "attributive particle"], vietnamese: ["của", "thuộc về"] },
-    { chinese: "工", pinyin: "gōng", english: ["work"], vietnamese: ["công", "công việc"] },
-    { chinese: "工人", pinyin: "gōng rén", english: ["worker"], vietnamese: ["công nhân"] },
-    { chinese: "左", pinyin: "zuǒ", english: ["left"], vietnamese: ["tả", "trái"] },
-    { chinese: "左手", pinyin: "zuǒ shǒu", english: ["left hand"], vietnamese: ["tả thủ", "tay trái"] },
-    { chinese: "右", pinyin: "yòu", english: ["right"], vietnamese: ["hữu", "phải"] },
-    { chinese: "右手", pinyin: "yòu shǒu", english: ["right hand"], vietnamese: ["hữu thủ", "tay phải"] },
-    { chinese: "左右", pinyin: "zuǒ yòu", english: ["around", "about"], vietnamese: ["khoảng", "xấp xỉ"] },
-    { chinese: "十", pinyin: "shí", english: ["ten"], vietnamese: ["thập", "mười"] },
-    { chinese: "什", pinyin: "shén", english: ["part of the word what"], vietnamese: [""] },
-    { chinese: "干", pinyin: "gān", english: ["dry", "to do"], vietnamese: ["can", "khô"] },
-    { chinese: "午", pinyin: "wǔ", english: ["noon", "mid-day"], vietnamese: ["ngọ", "buổi trưa"] },
-    { chinese: "上午", pinyin: "shàng wǔ", english: ["late morning"], vietnamese: ["buổi sáng"] },
-    { chinese: "中午", pinyin: "zhōng wǔ", english: ["noon"], vietnamese: ["giữa trưa"] },
-    { chinese: "下午", pinyin: "xià wǔ", english: ["afternoon"], vietnamese: ["buổi chiều"] },
-    { chinese: "牛", pinyin: "niú", english: ["beef", "cow"], vietnamese: ["ngưu", "con bò", "con trâu"] },
-    { chinese: "生", pinyin: "shēng", english: ["life", "birth"], vietnamese: ["sinh", "sinh đẻ"] },
-    { chinese: "八", pinyin: "bā", english: ["eight"], vietnamese: ["bát", "tám"] },
-    { chinese: "六", pinyin: "liù", english: ["six"], vietnamese: ["lục", "sáu"] },
-    { chinese: "分", pinyin: "fēn", english: ["distinguish","minute", "point"], vietnamese: ["phân", "phân chia", "phần"] },
-    { chinese: "十分", pinyin: "shí fēn", english: ["very"], vietnamese: ["rất"] },
-    { chinese: "几", pinyin: "jǐ", english: ["how many"], vietnamese: ["kỷ", "bao nhiêu"] },
-    { chinese: "儿", pinyin: "ér", english: ["son"], vietnamese: ["nhi", "đứa trẻ"] },
-    { chinese: "儿子", pinyin: "ér zi", english: ["son"], vietnamese: ["con trai"] },
-    { chinese: "九", pinyin: "jiǔ", english: ["nine"], vietnamese: ["cửu", "chín"] },
-    { chinese: "小", pinyin: "xiǎo", english: ["small", "little", "young"], vietnamese: ["tiểu", "nhỏ", "bé", "nhỏ bé"] },
-    { chinese: "小心", pinyin: "xiǎo xīn", english: ["careful"], vietnamese: ["cẩn thận"] },
-    { chinese: "大小", pinyin: "dà xiǎo", english: ["size"], vietnamese: ["kích cỡ", "kích thước"] },
-    { chinese: "从小", pinyin: "cóng xiǎo", english: ["since childhood", "since I was little"], vietnamese: ["từ nhỏ"] },
-    { chinese: "少", pinyin: "shǎo", english: ["few (quantity)", "lacking"], vietnamese: ["thiếu", "thiểu", "ít ỏi", "kém", "không đủ"] },
-    { chinese: "文", pinyin: "wén", english: ["wrting", "culture"], vietnamese: ["văn"] },
-    { chinese: "中文", pinyin: "zhōng wén", english: ["Chinese language"], vietnamese: ["Trung ngữ", "tiếng Trung"] },
-    { chinese: "文明", pinyin: "wén míng", english: ["civilization"], vietnamese: ["văn minh"] },
-    { chinese: "因", pinyin: "yīn", english: ["because", "reason"], vietnamese: ["nguyên nhân", "nhân"] },
-    { chinese: "回", pinyin: "huí", english: ["return", "back"], vietnamese: ["hối", "hồi", "về", "trở về"] },
-    { chinese: "四", pinyin: "sì", english: ["four"], vietnamese: ["tứ", "bốn"] },
-    { chinese: "不", pinyin: "bù", english: ["not"], vietnamese: ["bất", "không", "chẳng"] },
-    { chinese: "不少", pinyin: "bù shǎo", english: ["a lot"], vietnamese: ["nhiều", "không ít"] },
-    { chinese: "坏", pinyin: "huài", english: ["bad", "broken", "damaged"], vietnamese: ["hoài", "hoại", "tồi", "kém", "xấu", "hư", "hỏng", "ung"] },
-    { chinese: "坏人", pinyin: "huài rén", english: ["bad person"], vietnamese: ["người xấu"] },
-    { chinese: "坏了", pinyin: "huài le", english: ["broken", "damaged"], vietnamese: ["hư hỏng"] },
-    { chinese: "还", pinyin: "hái", english: ["still", "yet"], vietnamese: ["hoàn", "vẫn còn", "vẫn chưa"] },
-    { chinese: "七", pinyin: "qī", english: ["seven"], vietnamese: ["thất", "bảy"] },
-    { chinese: "七上八下", pinyin: "qī shàng bā xià", english: ["to feel worried"], vietnamese: ["thấp thỏm", "bồn chồn", "lo lắng", "bất an"] },
-    { chinese: "北", pinyin: "běi", english: ["north"], vietnamese: ["bắc", "phía bắc", "phương bắc"] },
-    { chinese: "北大", pinyin: "běi dà", english: ["Beijing University"], vietnamese: ["Đại học Bắc Kinh"] },
-    { chinese: "花", pinyin: "huā", english: ["flower", "to spend"], vietnamese: ["hoa", "bông hoa"] },
-    { chinese: "文化", pinyin: "wén huà", english: ["culture"], vietnamese: ["văn hóa"] },
-    { chinese: "乐", pinyin: "lè/yuè", english: ["happy", "music"], vietnamese: ["lạc", "nhạc"] },
-    { chinese: "东", pinyin: "dōng", english: ["east"], vietnamese: ["đông", "phía đông", "phương đông"] },
-    { chinese: "东北", pinyin: "dōng běi", english: ["northeast"], vietnamese: ["đông bắc"] },
-    { chinese: "车", pinyin: "chē", english: ["car", "vehicle"], vietnamese: ["xa", "xe"] },
-    { chinese: "上车", pinyin: "shàng chē", english: ["board the vehicle"], vietnamese: ["lên xe"] },
-    { chinese: "下车", pinyin: "xià chē", english: ["get off the vehicle"], vietnamese: ["xuống xe"] },
-    { chinese: "力", pinyin: "lì", english: ["power", "strength"], vietnamese: ["lực", "sức lực"] },
-    { chinese: "人力", pinyin: "rén lì", english: ["manpower"], vietnamese: ["nhân lực"] },
-    { chinese: "为", pinyin: "wèi", english: ["for"], vietnamese: ["vị"] },
-    { chinese: "为了", pinyin: "wèi le", english: ["in order to"], vietnamese: ["để", "để cho"] },
-    { chinese: "因为", pinyin: "yīn wéi", english: ["because"], vietnamese: ["bởi vì"] },
-    { chinese: "边", pinyin: "biān", english: ["side"], vietnamese: ["biên", "bên", "phía"] },
-    { chinese: "左边", pinyin: "zuǒ biān", english: ["left side"], vietnamese: ["bên trái"] },
-    { chinese: "右边", pinyin: "yòu biān", english: ["right side"], vietnamese: ["bên phải"] },
-    { chinese: "上边", pinyin: "shàng biān", english: ["top side"], vietnamese: ["bên trên"] },
-    { chinese: "下边", pinyin: "xià biān", english: ["bottom side"], vietnamese: ["bên dưới"] },
-    { chinese: "女", pinyin: "nǚ", english: ["woman", "female"], vietnamese: ["nữ", "đàn bà", "con gái"] },
-    { chinese: "如", pinyin: "rú", english: ["if"], vietnamese: ["như"] },
-    { chinese: "好", pinyin: "hǎo", english: ["good", "well", "fine"], vietnamese: ["tốt", "được", "hảo"] },
-    { chinese: "好人", pinyin: "hǎo rén", english: ["good person"], vietnamese: ["người tốt"] },
-    { chinese: "好的", pinyin: "hǎo de", english: ["OK", "alright"], vietnamese: ["được thôi", "vâng ạ"] },
-    { chinese: "不好不坏", pinyin: "bù hǎo bù huài", english: ["not distinguish good or bad"], vietnamese: ["bất phân tốt xấu"] },
-    { chinese: "不如", pinyin: "bù rú", english: ["not as good as", "not equal to"], vietnamese: ["không bằng"] },
-    { chinese: "儿女", pinyin: "ér nǚ", english: ["children", "sons and daughters"], vietnamese: ["con cái", "con trai và con gái"] },
-    { chinese: "也", pinyin: "yě", english: ["also", "too"], vietnamese: ["cũng", "cũng vậy", "dã"] },
-    { chinese: "地", pinyin: "dì", english: ["earth", "ground", "land"], vietnamese: ["đất", "mặt đất", "địa"] },
-    { chinese: "地下", pinyin: "dì xià", english: ["underground"], vietnamese: ["dưới lòng đất", "ngầm"] },
-    { chinese: "天地", pinyin: "tiān dì", english: ["sky and earth", "the world"], vietnamese: ["trời đất", "thiên địa"] },
-    { chinese: "有", pinyin: "yǒu", english: ["to have", "there is/are"], vietnamese: ["hữu", "có"] },
-    { chinese: "用", pinyin: "yòng", english: ["to use"], vietnamese: ["dụng", "dùng", "sử dụng"] },
-    { chinese: "用心", pinyin: "yòng xīn", english: ["attentive", "diligent", "with care"], vietnamese: ["dụng tâm", "chuyên tâm", "chú tâm", "tận tâm"] },
-    { chinese: "不用", pinyin: "bú yòng", english: ["don't need to", "no need"], vietnamese: ["không cần"] },
-    { chinese: "有用", pinyin: "yǒu yòng", english: ["useful"], vietnamese: ["hữu dụng", "có ích"] },
-    { chinese: "朋", pinyin: "péng", english: ["friend"], vietnamese: ["bằng", "bạn bè"] },
-];
+    { chinese: "万", pinyin: "wàn", english: ["ten thousands"], vietnamese: ["mười nghìn", "vạn"] },
+    { chinese: "万万", pinyin: "wàn wàn", english: ["absolutely"], vietnamese: ["nhất định", "tuyệt đối"] },
+    { chinese: "万一", pinyin: "wàn yī", english: ["just in case"], vietnamese: ["phòng khi/lỡ như", "phòng khi", "lỡ như", "vạn nhất"] },
+    { chinese: "万分", pinyin: "wàn fēn", english: ["absolutely", "extremely"], vietnamese: ["vô cùng/cực kỳ", "vô cùng", "cực kỳ", "vạn phần"] },
+    { chinese: "历", pinyin: "lì", english: ["experience"], vietnamese: ["lịch"] },
+    { chinese: "学历", pinyin: "xué lì", english: ["educational background"], vietnamese: ["trình độ học vấn", "học vấn"] },
+    { chinese: "来历", pinyin: "lái lì", english: ["background"], vietnamese: ["lai lịch"] },
+    { chinese: "日历", pinyin: "rì lì", english: ["calendar"], vietnamese: ["lịch", "cuốn lịch"] },
+    { chinese: "大方", pinyin: "dà fang", english: ["generous"], vietnamese: ["hào phóng", "rộng rãi"] },
+    { chinese: "千", pinyin: "qiān", english: ["a thousand"], vietnamese: ["nghìn", "ngàn", "thiên"] },
+    { chinese: "千万", pinyin: "qiān wàn", english: ["absolutely"], vietnamese: ["nhất định", "tuyệt đối"] },
+    { chinese: "之", pinyin: "zhī", english: ["of"], vietnamese: ["của"] },
+    { chinese: "之前", pinyin: "zhī qián", english: ["before"], vietnamese: ["trước đây", "trước khi", "trước"] },
+    { chinese: "之后", pinyin: "zhī hòu", english: ["after"], vietnamese: ["sau đó", "sau khi", "sau"] },
+    { chinese: "记得", pinyin: "jì de", english: ["remember"], vietnamese: ["nhớ lại"] },
+    { chinese: "记住", pinyin: "jì zhù", english: ["remember"], vietnamese: ["ghi nhớ"] },
+    { chinese: "于", pinyin: "yú", english: ["about", "therefore"], vietnamese: ["do đó", "vu"] },
+    { chinese: "于是", pinyin: "yú shì", english: ["therefore"], vietnamese: ["thế là", "vì vậy", "cho nên", "do đó"] },
+    { chinese: "关于", pinyin: "guān yú", english: ["about"], vietnamese: ["đối với", "liên quan đến", "về"] },
+    { chinese: "对于", pinyin: "duì yú", english: ["about"], vietnamese: ["đối với", "liên quan đến", "về"] },
+    { chinese: "等于", pinyin: "děng yú", english: ["equals"], vietnamese: ["bằng"] },
+    { chinese: "乎", pinyin: "hū", english: ["almost"], vietnamese: ["gần như", "hồ"] },
+    { chinese: "几乎", pinyin: "jī hū", english: ["almost"], vietnamese: ["hầu như", "gần như"] },
+    { chinese: "在乎", pinyin: "zài hu", english: ["care about"], vietnamese: ["quan tâm", "để tâm", "để ý"] },
+    { chinese: "卡", pinyin: "kǎ / qiā", english: ["card", "get stuck"], vietnamese: ["thẻ", "kẹt", "mắc kẹt"] },
+    { chinese: "卡车", pinyin: "kǎ chē", english: ["truck"], vietnamese: ["xe tải"] },
+    { chinese: "片", pinyin: "piàn", english: ["slice", "sheet"], vietnamese: ["miếng/lát/tấm", "phiến"] },
+    { chinese: "卡片", pinyin: "kǎ piàn", english: ["card"], vietnamese: ["tấm thẻ", "thiệp", "thẻ"] },
+    { chinese: "名片", pinyin: "míng piàn", english: ["business card"], vietnamese: ["danh thiếp"] },
+    { chinese: "止", pinyin: "zhǐ", english: ["stop", "end"], vietnamese: ["dừng lại", "kết thúc", "chỉ"] },
+    { chinese: "不止", pinyin: "bù zhǐ", english: ["not only", "more than"], vietnamese: ["không chỉ", "hơn"] },
+    { chinese: "到...为止", pinyin: "dào... wéi zhǐ", english: ["until"], vietnamese: ["cho đến khi", "cho đến"] },
+
+]
 
 // --- DOM ELEMENTS ---
 const questionDisplay = document.getElementById('question-display');
@@ -180,8 +84,12 @@ function loadNextWord() {
         questionDisplay.classList.remove('meaning');
         answerInput.placeholder = "Type meaning (EN or VI)...";
     } else { // 'meaning_to_char'
-        const isEnglish = Math.random() > 0.5;
-        const meaning = isEnglish ? currentWord.english[0] : currentWord.vietnamese[0];
+        // OLD
+        // const isEnglish = Math.random() > 0.5;
+        // const meaning = isEnglish ? currentWord.english[0] : currentWord.vietnamese[0];
+        
+        //NEW
+        const meaning = currentWord.vietnamese[0];
         questionDisplay.textContent = meaning;
         // pinyinDisplay is already cleared above, so nothing shows here
         questionDisplay.classList.add('meaning');
@@ -226,7 +134,7 @@ function handleCorrectAnswer() {
     setTimeout(() => {
         currentWordIndex++;
         loadNextWord();
-    }, 2400); // Increased delay slightly to allow time to see pinyin
+    }, 1800); // Increased delay slightly to allow time to see pinyin
 }
 
 function handleWrongAnswer() {
@@ -241,7 +149,11 @@ function handleWrongAnswer() {
         if (currentMode === 'char_to_meaning') {
             // ADDED: Show pinyin after 3 wrong attempts
             pinyinDisplay.textContent = currentWord.pinyin;
-            correctAnswerText = `${currentWord.english[0]} / ${currentWord.vietnamese[0]}`;
+            // OLD
+            // correctAnswerText = `${currentWord.english[0]} / ${currentWord.vietnamese[0]}`;
+
+            // NEW
+            correctAnswerText = currentWord.vietnamese[0];
         } else {
             correctAnswerText = currentWord.chinese;
         }
